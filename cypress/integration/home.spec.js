65886e5b8180d1cd6b10f00582634dab3d8cb064
Rui Neto <ruineto11@gmail.com>
2022-05-05 16:55:54 -0300

    test: add tests on home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -9,10 +9,121 @@ describe("Home page tests", () => {
 
     cy.visit("http://localhost:3000/");
 
-    cy.get('.name').type(music.name);
-    cy.get('.youtubeLink').type(music.youtubeLink);
-    cy.get('.submit').click();
+    cy.get('input[placeholder="Name"]').type(music.name);
+    cy.get('input[placeholder="https://youtu.be/..."]').type(music.youtubeLink);
+  
+    cy.intercept("POST", "http://localhost:5000/recommendations").as("createRecommendations");
+
+    cy.get('button').click();
+
+    cy.wait("@createRecommendations");
     
     cy.contains(music.name);
+
+    cy.end();
+  });
+
+  it("should return an alert when registering an existing song", () => {
+    const music = {
+      name: "Falamansa - Xote dos Milagres",
+      youtubeLink: "https://www.youtube.com/watch?v=chwyjJbcs1Y"
+    }
+
+    cy.visit("http://localhost:3000/");
+
+    cy.get('input[placeholder="Name"]').type(music.name);
+    cy.get('input[placeholder="https://youtu.be/..."]').type(music.youtubeLink);
+
+    cy.intercept("POST", "http://localhost:5000/recommendations").as("createRecommendations");
+
+    cy.get('button').click();
+
+    cy.wait("@createRecommendations");
+
+    cy.on('window:alert', (text) => {
+      expect(text).to.contains('Error creating recommendation!');
+    });
+
+    cy.end();
+  });
+
+  it("should return an alert when registering an invalid song", () => {
+    cy.get('button').click();
+
+    cy.visit("http://localhost:3000/");
+
+    cy.on('window:alert', (text) => {
+      expect(text).to.contains('Error creating recommendation!');
+    });
+
+    cy.end();
+  });
+
+  it("should increase recommendation counter", () => {
+    const music = {
+      name: "Falamansa - Xote dos Milagres",
+      youtubeLink: "https://www.youtube.com/watch?v=chwyjJbcs1Y"
+    }
+
+    cy.visit("http://localhost:3000/");
+
+    cy.get('input[placeholder="Name"]').type(music.name);
+    cy.get('input[placeholder="https://youtu.be/..."]').type(music.youtubeLink);
+
+    cy.intercept("POST", "http://localhost:5000/recommendations").as("createRecommendations");
+
+    cy.get('button').click();
+
+    cy.wait("@createRecommendations");
+
+    cy.contains(music.name).get('article').within(() => {
+      cy.get('div:nth-child(3)').should('have.text',"0")
+    });
+
+    cy.contains(music.name).get('article').within(() => {
+      cy.get('svg:nth-child(1)').click();
+    });
+
+    cy.reload();
+
+    cy.contains(music.name).get('article').within(() => {
+      cy.get('div:nth-child(3)').should('have.text',"1")
+    });
+
+    cy.end();
+  });
+
+  it("should decrease recommendation counter", () => {
+    const music = {
+      name: "Falamansa - Xote dos Milagres",
+      youtubeLink: "https://www.youtube.com/watch?v=chwyjJbcs1Y"
+    }
+
+    cy.visit("http://localhost:3000/");
+
+    cy.get('input[placeholder="Name"]').type(music.name);
+    cy.get('input[placeholder="https://youtu.be/..."]').type(music.youtubeLink);
+
+    cy.intercept("POST", "http://localhost:5000/recommendations").as("createRecommendations");
+
+    cy.get('button').click();
+
+    cy.wait("@createRecommendations");
+
+    cy.contains(music.name).get('article').within(() => {
+      cy.get('div:nth-child(3)').should('have.text',"1")
+    });
+
+    cy.contains(music.name).get('article').within(() => {
+      cy.get('svg:nth-child(2)').click();
+    });
+
+    cy.reload();
+
+    cy.contains(music.name).get('article').within(() => {
+      cy.get('div:nth-child(3)').should('have.text',"0")
+    });
+
+    cy.end();
   });
 });
--- src/components/CreateNewRecommendation/CreateNewRecommendation.js
+++ src/components/CreateNewRecommendation/CreateNewRecommendation.js
@@ -18,9 +18,9 @@ export default function CreateNewRecommendation({ onCreateNewRecommendation = ()
   
   return (
     <Container>
-      <Input className="name" type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} disabled={disabled} />
-      <Input className="youtubeLink" type="text" placeholder="https://youtu.be/..." value={link} onChange={e => setLink(e.target.value)} disabled={disabled} />
-      <Button className="submit" onClick={() => handleCreateRecommendation()} disabled={disabled}>
+      <Input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} disabled={disabled} />
+      <Input type="text" placeholder="https://youtu.be/..." value={link} onChange={e => setLink(e.target.value)} disabled={disabled} />
+      <Button onClick={() => handleCreateRecommendation()} disabled={disabled}>
         <IoReturnUpForwardOutline size="24px" color="#fff" />
       </Button>
     </Container>
